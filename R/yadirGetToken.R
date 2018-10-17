099e0789c4833a21b9cad7660aac35f3c7546444
test-rf <44220953+test-rf@users.noreply.github.com>
2018-10-17 15:33:06 +0700

    Update yadirGetToken.R

--- R/yadirGetToken.R
+++ R/yadirGetToken.R
@@ -1,6 +1,6 @@
 yadirGetToken <-
 function(){
-  browseURL("https://oauth.yandex.ru/authorize?response_type=token&client_id=dd90e90c94504d23a9ed396473a538a9")
+  browseURL("https://oauth.yandex.ru/authorize?response_type=token&client_id=")
   token <- readline(prompt = "Enter your token: ")
   return(token)
 }
